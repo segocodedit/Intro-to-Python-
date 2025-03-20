fc4196a0c763c50247ee0f906960120a32446985
masego <masegokhola@icloud.com>
2025-03-20 18:39:35 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,20 +3,17 @@ x = float(input("Enter a number: "))
 y = float(input("Enter another number: "))
 op = str(input("Enter operator (+, -, *, /) "))
 
-#round of answer to 2 decimals
-roundof = 2
-
 if (op == "+"): 
     sum = x+y
-    print(f"{x} {op} {y} = {round(sum, roundof)}")
+    print(f"{x} {op} {y} = {round(sum, 2)}") #round(sum, 2) rounds of to two decimals
    
 elif (op == "-"): 
     sum = x-y
-    print(f"{x} {op} {y} = {round(sum, roundof)}")
+    print(f"{x} {op} {y} = {round(sum, 2)}")
 
 elif (op == "*"): 
     sum = x*y
-    print(f"{x} {op} {y} = {round(sum, roundof)}")
+    print(f"{x} {op} {y} = {round(sum, 2)}")
     
 elif (op == "/"):
     sum = x/y
@@ -25,8 +22,6 @@ elif (op == "/"):
 
 else:
     print("Invalid operator")
-
-
 
 
 
